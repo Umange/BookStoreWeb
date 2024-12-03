3e18db82c6cbf38fc140e61defb4fd7a35a93969
Umange <umangkumawat612@gmail.com>
2024-12-03 11:27:32 +0530

    Initial commit

---
+++ BookStore/Frontend/src/components/Course.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4000/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+          Paid courses offer in-depth, structured learning experiences curated by industry professionals or experts. They often include comprehensive content, practical exercises, certifications, and personalized support to help learners achieve their goals. 
+          Investing in a paid course provides access to premium resources and ensures high-quality education tailored to your needs.
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
